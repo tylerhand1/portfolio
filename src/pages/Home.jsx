a38e80613ff80bb62c1aba7d4ee62ff0b79c9b55
tylerhand1 <tyha7462@colorado.edu>
2024-04-23 13:08:18 -0600

    Started creating content for Home.jsx Removed list-style reset from * and put in header ul

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,14 @@ const Home = () => {
   return (
     <>
       <Navbar isHome={true} />
-      <main>
+      <main className="flex">
         <h1>Home</h1>
+        <div className="card flex">
+          <h2>About Me</h2>
+          <ul>
+            <li>Current student at CU Boulder</li>
+          </ul>
+        </div>
       </main>
     </>
   );
