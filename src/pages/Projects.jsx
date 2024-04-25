5029f9731178bec64dd7b27f70e802b169e82b1e
tylerhand1 <tyha7462@colorado.edu>
2024-04-25 12:31:10 -0600

    Created basic grid layout for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,8 +4,34 @@ const Projects = () => {
   return (
     <>
       <Navbar isProjects={true} />
-      <main>
+      <main className="flex">
         <h1>Projects</h1>
+        <div className="project-container grid">
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+          <div className="project-card">
+
+          </div>
+        </div>
       </main>
     </>
   );
