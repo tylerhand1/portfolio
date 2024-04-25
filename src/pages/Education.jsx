4c171ce7decfd1520757e8d167b661a008a60a38
tylerhand1 <tyha7462@colorado.edu>
2024-04-25 13:12:14 -0600

    Added degree info title to Education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -8,6 +8,7 @@ const Education = () => {
         <div className="card">
           <h1>Education</h1>
           <h2>University of Colorado Boulder</h2>
+          <h3>Bachelor of Science in Computer Science</h3>
           <p>Expected graduation: May 2024</p>
           <p>Prior to starting my education journey at CU Boulder, I was always interested in programming
             and web development. Because of this passion, I chose to pursue a Bachelor&apos;s of Science 
