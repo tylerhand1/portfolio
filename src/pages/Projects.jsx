b87b28ca25365aee008153994bdbbf5e06d85ccf
tylerhand1 <tyha7462@colorado.edu>
2024-04-25 13:11:44 -0600

    Converted Projects.jsx to use Project component in project-container

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar';
+import Project from '../components/Project';
 
 const Projects = () => {
   return (
@@ -7,30 +8,11 @@ const Projects = () => {
       <main className="flex">
         <h1>Projects</h1>
         <div className="project-container grid">
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
-          <div className="project-card">
-
-          </div>
+          <Project title={'Biology Lab Simulator'} info={'Collaborated with a team of six to build a biology lab simulator for BioFrontiers Institute'} />
+          <Project title={'HackCU24 Sat-Track'} info={'Real-time satellite tracker'} />
+          <Project title={'Proxy Web Server'} info={'A proxy web server written in C'} />
+          <Project title={'Connect Four AI'} info={'AI Connect Four player using the alpha-beta algorithm'} />
+          <Project title={'Doodle Designer'} info={'A 2D web-based painter with image saving capabilities'} />
         </div>
       </main>
     </>
