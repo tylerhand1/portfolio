ae7edb8bafad74381e3a69cdde2f024f623b5f15
tylerhand1 <tyha7462@colorado.edu>
2024-04-28 18:09:17 -0600

    Create project files

---
+++ src/pages/projects/ConnectFourAI.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../components/Navbar';
+
+const ConnectFourAI = () => {
+  return (
+    <>
+      <Navbar isProjects={true} />
+      <main className="flex">
+        <h1>Connect Four AI</h1>
+      </main>
+    </>
+  );
+};
+
+export default ConnectFourAI;
---
+++ src/pages/projects/SatTrack.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../components/Navbar';
+
+const SatTrack = () => {
+  return (
+    <>
+      <Navbar isProjects={true} />
+      <main className="flex">
+        <h1>Sat-Track</h1>
+      </main>
+    </>
+  );
+};
+
+export default SatTrack;
