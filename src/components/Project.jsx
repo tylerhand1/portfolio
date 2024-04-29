0d3dab0387652fe6dd95ec89be573cd8dda92f35
tylerhand1 <tyha7462@colorado.edu>
2024-04-28 18:09:33 -0600

    Configured React Router for project list

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,27 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+
 const Project = ({ img, imgAlt, title, info }) => {
+  let baseLink = '/projects';
+  let titleLink = title.replace(/ /g, '-');
+
   if (img && imgAlt) {
     return (
       <div className="project-card">
-        <a className="flex" href={`projects/${title}`}>
+        <Link className="flex" to={`${baseLink}/${titleLink}`}>
           <img src={img} alt={imgAlt} />
           <h4>{title}</h4>
           <p>{info}</p>
-        </a>
+        </Link>
       </div>
     );
   }
   return (
     <div className="project-card">
-      <a className="flex" href={`projects/${title}`}>
+      <Link className="flex" to={`${baseLink}/${titleLink}`}>
         <h4>{title}</h4>
         <p>{info}</p>
-      </a>
+      </Link>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,28 @@ import Home from './pages/Home';
 import Education from './pages/Education';
 import Projects from './pages/Projects';
 
+// Project Pages
+import BioLabSim from './pages/projects/BioLabSim';
+import ConnectFourAI from './pages/projects/ConnectFourAI';
+import DoodleDesigner from './pages/projects/DoodleDesigner';
+import ProxyWebServer from './pages/projects/ProxyWebServer';
+import SatTrack from './pages/projects/SatTrack';
+
 ReactDOM.createRoot(document.getElementById('root')).render( 
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to={'/home'} />}/>
-        <Route path='/home' element={<Home />}/>
-        <Route path="/education" element={<Education />}/>
-        <Route path="projects" element={<Projects />}/>
+        <Route path="/" element={<Navigate to={'/home'} />} />
+        <Route path='/home' element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/projects" element={<Projects />} />
+
+        {/* Projects */}
+        <Route path={'/projects/Biology-Lab-Simulator'} element={<BioLabSim />} />
+        <Route path={'/projects/HackCU24-Sat-Track'} element={<SatTrack />} />
+        <Route path={'/projects/Proxy-Web-Server'} element={<ProxyWebServer />} />
+        <Route path={'/projects/Connect-Four-AI'} element={<ConnectFourAI />} />
+        <Route path={'/projects/Doodle-Designer'} element={<DoodleDesigner />} />
       </Routes>
     </Router>
   </React.StrictMode>
