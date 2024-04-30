97c8d2827d0bd08bd6f0a75cc5b073300c299dff
tylerhand1 <tyha7462@colorado.edu>
2024-04-30 13:15:56 -0600

    Started creating layout for About Me section on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,22 @@ const Home = () => {
     <>
       <Navbar isHome={true} />
       <main className='flex'>
-        <div className='card'>
+        <div className='card home-card'>
           <h1>About Me</h1>
-          <p>I am a computer science student.</p>
+          <div className="grid home-card-sections-container">
+            <div className="home-card-section">
+              <h2>My Experience</h2>
+              {/* TODO: Add experience into this section */}
+            </div>
+            <div className="home-card-section">
+              <h2>Technologies</h2>
+              {/* TODO: Add svg icons for each technology I wish to list */}
+            </div>
+            <div className="home-card-section">
+              <h2>Interests</h2>
+              {/* Add interests in, possibly in ul format */}
+            </div>
+          </div>
         </div>
       </main>
     </>
