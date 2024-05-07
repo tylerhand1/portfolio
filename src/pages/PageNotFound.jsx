c9a0d98c5a9f96de4357e8b5ba91b41e5733fbe5
tylerhand1 <tyha7462@colorado.edu>
2024-05-07 05:12:06 -0600

    Updated PageNotFound to have instructions and styling

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -4,8 +4,11 @@ const PageNotFound = () => {
   return (
     <>
       <Navbar isProjects={true} />
-      <main className='flex project-container-root'>
-        <h1>Error 404</h1>
+      <main className='flex'>
+        <div className='card err-404-card'>
+          <h1>Error 404</h1>
+          <p>Page not found. Please use the above navigation menu.</p>
+        </div>
       </main>
     </>
   );
