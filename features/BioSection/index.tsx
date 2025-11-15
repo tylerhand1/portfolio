881c68ee0074eee1daf3a2c3ea65819a5703f974
Tyler Hand <tylerhand135@gmail.com>
2025-11-14 22:10:26 -0700

    Add highlight to name and italic to subtext

--- features/BioSection/index.tsx
+++ features/BioSection/index.tsx
@@ -3,12 +3,12 @@ const BioSection = () => (
     <div className="text-balance text-center">
       <div className="flex flex-col gap-2 md:gap-4">
         <h1 className="font-black text-3xl md:text-5xl lg:text-7xl">
-          Hi, I&apos;m Tyler Hand
+          Hi, I&apos;m <span className="text-highlight">Tyler Hand</span>
         </h1>
         <h2 className="font-bold text-fg-secondary leading-tight text-xl md:text-2xl lg:text-4xl">
           I am a software engineer with two years experience.
         </h2>
-        <h3 className="text-fg-tertiary font-light leading-snug sm:leading-normal sm:text-lg lg:text-xl">
+        <h3 className="text-fg-tertiary italic font-light leading-snug sm:leading-normal sm:text-lg lg:text-xl">
           With a specialization in full-stack web development using Next.js,
           React and Express and expertise in TypeScript, Tailwind CSS, modern
           database practices and CI/CD, I build and deploy applications from
