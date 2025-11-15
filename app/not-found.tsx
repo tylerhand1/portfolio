e60d0841b47d7a37fdddb584959bacf3b52e63bc
Tyler Hand <tylerhand135@gmail.com>
2025-11-15 12:23:45 -0700

    Fix up not found to look similar to home splash

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <div>
-      <h1 className="font-medium text-2xl">404 - Not Found</h1>
-      <p className="text-fg-secondary text-lg">
+    <div className="h-dvh flex flex-col justify-center items-center">
+      <h1 className="font-medium text-2xl md:text-4xl lg:text-6xl">
+        404 - Not Found
+      </h1>
+      <p className="text-fg-secondary text-lg md:text-2xl lg:text-4xl">
         Could not find requested resource
       </p>
     </div>
